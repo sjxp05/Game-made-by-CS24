62bc93e91c2a39cb79fbf75397a80043e1cdac62
jihi210 <jihi@uos.ac.kr>
2025-04-07 14:13:18 +0900

    database structure

---
+++ DataBase/app/src/main/java/irumaeDB/Converters.java
@@ -0,0 +1,17 @@
+package irumaeDB;
+
+import androidx.room.TypeConverter;
+/*
+Major <-> String 변환기
+ */
+public class Converters {
+    @TypeConverter
+    public static Major fromString(String value) {
+        return value == null ? null : Major.valueOf(value);
+    }
+
+    @TypeConverter
+    public static String fromEnum(Major major) {
+        return major == null ? null : major.name();
+    }
+}
